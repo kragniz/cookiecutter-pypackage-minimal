221154da0f79fb8b7094d0ca4647a2af66e89d8a
Patrick Stegmann <wonderb0lt@users.noreply.github.com>
2015-04-15 10:33:41 +0200

    Don't remove whitespaces for maintainer name
    This leads to the output to be formatted like this:
    
        __author__ = 'abc'__maintainer__ = 'xyz'
    
    which is a syntax error

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = '{{ cookiecutter.package_version }}'
 __author__ = '{{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>'
-{%- if cookiecutter.maintainer_name -%}
+{%- if cookiecutter.maintainer_name %}
 __maintainer__ = '{{ cookiecutter.maintainer_name }} <{{ cookiecutter.maintainer_email }}>'
 {%- endif %}
 __all__ = []
